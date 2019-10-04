85b6b5687e6b441fb1aa22897d6a2c28778d1311
grizzle <firbigi1993@gmail.com>
2019-10-05 03:53:49 +0900

    header-media query

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,80 @@ import React from 'react';
 import logo from 'img/logo.svg';
 import {Link, NavLink} from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <header>
-      <Link to = "/" className="logo-header"><img src = {logo} alt = "logo" /></Link>
-      <div className = "con-header">
-        <div className="header-info">
-          <div className="user-info">
-            <h3 className="user-team">design</h3>
-            <h4 className = "user-name">Taewoong</h4>
+class Header extends React.Component {
+
+  state = {
+    menu: false,
+    nowpage: "/"
+  }
+
+  toggleMenu = () => {
+    const newMenu = !this.state.menu;
+    this.setState({
+      menu: newMenu
+    })
+  }
+
+  changeNowPage = (e) => {
+    let nowpage = e.target.innerHTML;
+    if(nowpage === "home"){
+      nowpage = "/"
+    }
+
+    this.setState({
+      nowpage: nowpage
+    })
+  }
+
+  handleLogoClick = () => {
+    this.toggleMenu();
+  }
+
+  handleMenuClick = (e) => {
+    this.toggleMenu();
+    this.changeNowPage(e);
+  }
+
+
+  render() {
+    return (
+      <header>
+        <Link to = {window.innerWidth > 576 ? "/" : this.state.nowpage} className="logo-header" onClick = {this.handleLogoClick}><img src = {logo} alt = "logo" /></Link>
+
+        <div className = "con-header">
+          {/* gnb - web */}
+          <div className = "gnb">
+            <div className="header-info">
+              <div className="user-info">
+                <h3 className="user-team">design</h3>
+                <h4 className = "user-name">Taewoong</h4>
+              </div>
+            </div>
+
+            <nav className="nav-web">
+              <ul>
+                <li>
+                  <NavLink to = "/scrap" >scrap</NavLink>
+                </li>
+                <li><NavLink to = "/project" >project</NavLink></li>
+              </ul>
+            </nav>
           </div>
+
+          {/* gnb - movile */}
+          <nav className={this.state.menu ? "nav-mobile nav-mobile-active" : "nav-mobile"}>
+            <ul>
+              <li><NavLink exact to = "/" onClick = {this.handleMenuClick} >home</NavLink></li>
+              <li><NavLink to = "/scrap"  onClick = {this.handleMenuClick} >scrap</NavLink></li>
+              <li><NavLink to = "/project"  onClick = {this.handleMenuClick} >project</NavLink></li>
+            </ul>
+          </nav>
         </div>
 
-        <nav>
-          <ul>
-            <li><NavLink to = "/scrap" >scrap</NavLink></li>
-            <li><NavLink to = "/project" >project</NavLink></li>
-          </ul>
-        </nav>
-
-      </div>
-    </header>
-  );
+        
+      </header>
+    );
+  }
 }
 
 export default Header;
