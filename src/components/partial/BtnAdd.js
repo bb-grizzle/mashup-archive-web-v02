5b7e8ea02b999b7f116169f36e196c71ca7f0683
grizzle <firbigi1993@gmail.com>
2019-10-06 18:26:59 +0900

    fix hover on device

--- src/components/partial/BtnAdd.js
+++ src/components/partial/BtnAdd.js
@@ -2,7 +2,7 @@ import React from 'react';
 const BtnAdd = () => {
   return (
     <div className = "btn btn-add">
-      <div className = "btn-add-contetns">
+      <div className = "btn-add-contents">
         <div className="add-v add-line"></div>
         <div className="add-h add-line"></div>
       </div>
